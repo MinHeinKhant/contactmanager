3bb1fcf5cb78c12e8ce680c789b08e1a3efa3e22
minheinkhant <aungkoung007@gmail.com>
2018-10-31 04:20:30 +0630

    Updates

---
+++ precache-manifest.b2d53eee6668a366e071bb840f4143b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "6fe543055fc07e1fc2e2",
+    "url": "/contactmanager/static/js/main.6fe54305.chunk.js"
+  },
+  {
+    "revision": "ccffbefbd99512265c9b",
+    "url": "/contactmanager/static/js/1.ccffbefb.chunk.js"
+  },
+  {
+    "revision": "6fe543055fc07e1fc2e2",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "ccffbefbd99512265c9b",
+    "url": "/contactmanager/static/css/1.c2a45b3c.chunk.css"
+  },
+  {
+    "revision": "91a449b4ef5ac0e18a88516430b5299d",
+    "url": "/contactmanager/index.html"
+  }
+];
