965bc30dcd6de31cd3c0db00f4b97b77756edb04
minheinkhant <aungkoung007@gmail.com>
2018-10-31 04:09:19 +0630

    initial commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import NotFound from './components/pages/NotFound';
+import Contacts from './components/contacts/Contacts';
+import Header from './components/layout/Header';
+import AddContact from './components/contacts/AddContact';
+import EditContact from './components/contacts/EditContact';
+import About from './components/pages/About';
+import { Provider } from './Context';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <BrowserRouter>
+          <div className="App">
+            <Header branding="Contact Manager" />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route path="/about" component={About} />
+                <Route path="/contact/add" component={AddContact} />
+                <Route path="/contact/edit/:id" component={EditContact} />
+                <Route component={NotFound} />
+              </Switch>
+            </div>
+          </div>
+        </BrowserRouter>
+      </Provider>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/contacts/Contact.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Consumer } from '../../Context';
+import axios from 'axios';
+
+class Contact extends Component {
+  state = {
+    showContactInfo: false
+  };
+
+  onDeleteClick = async (id, dispatch) => {
+    await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+    dispatch({ type: 'DELETE_CONTACT', payload: id });
+  };
+
+  onEditClick = () => {};
+
+  render() {
+    const { showContactInfo } = this.state;
+    const { name, email, phone, id } = this.props.contact;
+
+    return (
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card card-body mb-3">
+              <h4>
+                {name}{' '}
+                <i
+                  onClick={() =>
+                    this.setState({ showContactInfo: !showContactInfo })
+                  }
+                  className="fas fa-sort-down"
+                  style={{ cursor: 'pointer' }}
+                />
+                <i
+                  onClick={this.onDeleteClick.bind(this, id, dispatch)}
+                  className="fas fa-times"
+                  style={{ cursor: 'pointer', color: 'red', float: 'right' }}
+                />
+                <Link to={`contact/edit/${id}`}>
+                  <i
+                    className="fas fa-pencil-alt"
+                    style={{
+                      cursor: 'pointer',
+                      color: 'green',
+                      float: 'right',
+                      marginRight: '1rem'
+                    }}
+                  />
+                </Link>
+              </h4>
+              {showContactInfo && (
+                <ul className="list-group">
+                  <li className="list-group-item">Email: {email}</li>
+                  <li className="list-group-item">Phone: {phone}</li>
+                </ul>
+              )}
+            </div>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+
+Contact.propTypes = {
+  contact: PropTypes.object.isRequired
+};
+
+export default Contact;
